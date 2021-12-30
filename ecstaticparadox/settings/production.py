30fa83e43eaf1fd7e438239b6913c8d16b1e5b75
Yubraj Bhandari <yubraj.bhandari.hero@gmail.com>
2021-12-30 14:14:13 +0545

    production ready

--- ecstaticparadox/settings/production.py
+++ ecstaticparadox/settings/production.py
@@ -1,7 +1,10 @@
 from .base import *
+import os
 
+SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
-
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 try:
     from .local import *
 except ImportError:
