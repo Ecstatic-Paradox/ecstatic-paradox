61643d552151d53d29010060533942ccde5b73cd
Yubraj Bhandari <yubraj.bhandari.hero@gmail.com>
2022-01-11 13:13:52 +0545

    setup mysql for production

--- ecstaticparadox/settings/base.py
+++ ecstaticparadox/settings/base.py
@@ -93,9 +93,18 @@ WSGI_APPLICATION = "ecstaticparadox.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    # }
+    'default': {
+        'NAME': 'periergi_ecstatic_paradox',
+        'ENGINE': 'mysql.connector.django',
+        'USER': os.environ['DB_USERNAME'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'OPTIONS': {
+          'autocommit': True,
+        },
     }
 }
 
