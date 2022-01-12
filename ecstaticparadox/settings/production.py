e38c57b7ad538662ed72f7c90b92e91652e5ffd5
Diwash Lamichhane <imdiwash.mrprogrammer@gmail.com>
2022-01-12 13:46:53 +0545

    added smtp

--- ecstaticparadox/settings/production.py
+++ ecstaticparadox/settings/production.py
@@ -2,6 +2,9 @@ from ecstaticparadox.settings.dev import ALLOWED_HOSTS
 from .base import *
 import os
 
+
+
+
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 CSRF_COOKIE_SECURE = False
@@ -9,6 +12,14 @@ SESSION_COOKIE_SECURE = False
 ALLOWED_HOSTS = ['app.ecstaticparadox.com', 'ecstaticparadox.com']
 CORS_ORIGIN_WHITELIST = ("https://ecstaticparadox.com",)
 WAGTAILAPI_BASE_URL = "https://app.ecstaticparadox.com"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "mail.ecstaticparadox.com"
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER = "noreply@ecstaticparadox.com" 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "../app.ecstaticparadox.com/media")
